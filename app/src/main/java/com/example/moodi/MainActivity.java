4c158fbe95fc0b91155404fa8cd355a6d6030e5c
nicklass <nicklas.stellberg@metropolia.fi>
2020-04-27 16:39:36 +0300

    Nappien linkitys/uudelleen nimeäminen ja lisää activityjä

--- app/src/main/java/com/example/moodi/MainActivity.java
+++ app/src/main/java/com/example/moodi/MainActivity.java
@@ -6,17 +6,46 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView sleepText;
+    private ProgressBar sleepBar;
+    private SeekBar sleepSeek;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sleepText= (TextView) findViewById(R.id.sleepTv);
+        sleepSeek= (SeekBar) findViewById(R.id.sleepSeek);
+        sleepSeek.setMax(24);
+
+        sleepSeek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int unet, boolean fromUser) {
+                sleepText.setText("" +unet +"h");
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
-    /** Called when the user taps the Send button */
+    /** Called when the user taps the button */
     public void barChart(View view) {
         Intent intent = new Intent(this, BarChartActivity.class);
         startActivity(intent);
