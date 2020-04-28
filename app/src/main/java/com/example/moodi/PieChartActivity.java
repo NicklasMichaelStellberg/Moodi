a57c345b878c0e97edc2f77237b33eee80e6a445
janiturp <60131919+janiturp@users.noreply.github.com>
2020-04-28 16:07:55 +0300

    PieChart testi.

--- app/src/main/java/com/example/moodi/PieChartActivity.java
+++ app/src/main/java/com/example/moodi/PieChartActivity.java
@@ -1,17 +1,54 @@
 package com.example.moodi;
 
+import androidx.annotation.ColorInt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PieChartActivity extends AppCompatActivity {
+    PieChart pieChart;
+    int value;
+    String label;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pie_chart);
+        pieChart = findViewById(R.id.pieChart);
+        List<PieEntry> entries = new ArrayList<>();
+
+        pieChart.setBackgroundColor(20991);
+
+        entries.add(new PieEntry(10, "Testi label 1"));
+        entries.add(new PieEntry(20, "Testi Label 2"));
+        entries.add(new PieEntry(40, "Testi label 3"));
+        entries.add(new PieEntry(30, "Testi label 4"));
+
+        PieDataSet set = new PieDataSet(entries, "Piirastesti");
+        set.setColors(ColorTemplate.COLORFUL_COLORS);
+        PieData data = new PieData(set);
+        pieChart.setData(data);
+        pieChart.setBackgroundColor(0x0051FF);
+        pieChart.invalidate();;
+
+
+    }
+
+
+    public void PieEntry (int value, String label){
+        this.value = value;
+        this.label = label;
     }
     public void mainActivity(View view) {
         Intent intent = new Intent(this, MainActivity.class);
